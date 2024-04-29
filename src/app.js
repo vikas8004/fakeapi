089285041555fde45bd09a59bb65fd8a7bec284c
vikas <vikas80046@gmail.com>
2024-04-29 12:01:57 +0530

    made some chnages

--- src/app.js
+++ src/app.js
@@ -5,4 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1", employeeRouter);
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "this is home page" });
+});
 export default app;
