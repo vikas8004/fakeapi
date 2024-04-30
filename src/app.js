8fe06da45253bac1005776e49d3d37beb156ebbd
vikas <vikas80046@gmail.com>
2024-04-30 11:02:09 +0530

    made some chnages

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import employeeRouter from "./routers/employee.router.js";
+import employeeRouter from "./routes/employee.router.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
